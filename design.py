da86efa7b7964dea997fd3cb870a62ad0ab7c06e
Kumaran <mkumaran.anand@gmail.com>
2020-12-21 14:40:12 +0000

    design exercise done

---
+++ design.py
@@ -0,0 +1,15 @@
+print('+ - - - - +', end=" "),
+print("- - - - +")
+def inclu():
+        i = 0
+        while i <= 4:
+            print("|", end= " "),
+            print("        |", end=" "),
+            print("        |")
+            i = i+1
+        print('+ - - - - +', end=" "),
+        print("- - - - +")
+inclu()
+def do_last():
+   inclu()
+do_last()
